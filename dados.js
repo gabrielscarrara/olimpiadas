14ca68d01c20d0f9780c97204cb71082f1b2b398
gabrielscarrara <97469316+gabrielscarrara@users.noreply.github.com>
2024-09-06 11:05:10 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,21 @@
+let dados = [
+    {
+        titulo: 'Rayssa Leal',
+        descricao: 'Rayssa Leal, conhecida como "Fadinha", é uma skatista brasileira que conquistou o mundo com seu talento e carisma.',
+        link: "https://pt.wikipedia.org/wiki/Rayssa_Leal",
+        tags: 'skate fada sk8'
+    },
+    {
+        titulo: 'Beatriz Souza',
+        descricao: 'Beatriz Souza é uma judoca brasileira que se destaca pela sua técnica e força, conquistando diversas medalhas em competições nacionais e internacionais.',
+        link: "https://pt.wikipedia.org/wiki/Beatriz_Souza",
+        tags: 'judo judô judoka'
+    },
+    {
+        titulo: 'Rebeca Andrade',
+        descricao: 'Rebeca Andrade é uma ginasta brasileira que se tornou um ícone do esporte, conquistando diversas medalhas em competições de alto nível, incluindo os Jogos Olímpicos.',
+        link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade",
+        tags: 'ginastica ginástica ouro'
+    }
+];
+
